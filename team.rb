b0be963e0c0580d14ad4a23fbc0014f89f2229fb
m.kotoma <makoto.866281@outlook.com>
2023-11-26 08:36:53 +0000

    空行なくす

--- team.rb
+++ team.rb
@@ -5,7 +5,7 @@ class Team
   attr_accessor :name, :win, :lose, :draw
 
   # インスタンスを初期化するための、特別なメソッド
-  def initialize(input_name, input_win, input_lose, input_draw)
+  def initialize(input_name, input_win, input_lose, input_draw)# ここはわざわざinput_をつけて別名にする必要なかった
     self.name = input_name
     self.win = input_win
     self.lose = input_lose
@@ -18,7 +18,9 @@ class Team
   end
   
   def show_team_result
-    puts self.name + "の2020年の成績は " + self.win.to_s + "勝" + self.lose.to_s + "敗" + self.draw.to_s + "分、勝率は" + self.calc_win_rate.to_s + "です。"
+    # ここもっとシンプルな書き方あったな、"#{self.name} です"みたいなの
+    #puts self.name + "の2020年の成績は " + self.win.to_s + "勝" + self.lose.to_s + "敗" + self.draw.to_s + "分、勝率は" + self.calc_win_rate.to_s + "です。"
+    puts "#{self.name}の2020年の成績は#{self.win.to_s}勝#{self.lose.to_s}敗#{self.draw.to_s}分、勝率は#{self.calc_win_rate.to_s}です。"
   end
 
 end
@@ -34,9 +36,9 @@ Swallows = Team.new('Swallows', 41, 69, 10)
 
 
 # インスタンスの使用
-puts Giants.show_team_result
-puts Tigers.show_team_result
-puts Dragons.show_team_result
-puts BayStars.show_team_result
-puts Carp.show_team_result
-puts Swallows.show_team_result
+print Giants.show_team_result
+print Tigers.show_team_result
+print Dragons.show_team_result
+print BayStars.show_team_result
+print Carp.show_team_result
+print Swallows.show_team_result
